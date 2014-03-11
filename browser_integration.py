c40088b810215cdd7543303a29064703106282ca
Alejandro Piad <alepiad@gmail.com>
2014-03-11 19:22:57 -0400

    Initial commit
    Already working:
    * Firing up Chrome
    * Reloading the browser
    * Navigating to a specific URL
    * Highlighting elements by CSS selector
    * Evaluating a JavaScript snippet
    
    To be added soon:
    * Automatic reload on save
    * Live CSS editing

---
+++ browser_integration.py
@@ -0,0 +1,195 @@
+import sublime
+import sublime_plugin
+import os
+import sys
+import threading
+import time
+
+from BrowserIntegration.selenium.webdriver import Chrome
+
+
+SETTINGS_FILE = 'BrowserIntegration.sublime-settings'
+chrome = None
+
+
+def async(function):
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=function, args=args, kwargs=kwargs)
+
+        try:
+            thread.start()
+            return thread
+        except Exception as e:
+            warning(str(e))
+
+    return wrapper
+
+
+def setting(cmd, setting, default=None):
+        settings = sublime.load_settings(SETTINGS_FILE)
+
+        if hasattr(cmd, 'view'):
+            view_settings = cmd.view.settings()
+        else:
+            view_settings = {}
+
+        global_value = settings.get(setting, default)
+        local_value = view_settings.get(setting, global_value)
+
+        return local_value
+
+
+def status(msg):
+    sublime.status_message("BrowserIntegration :: %s" % msg)
+
+
+def warning(msg):
+    sublime.status_message("(!) BrowserIntegration :: %s" % msg)
+
+
+@async
+def loading(msg, delay=1):
+    start = time.time()
+    count = 0
+    up = True
+
+    while time.time() - start < delay:
+        load = "[" + " " * count + "=" + " " * (5 - count) + "]"
+        sublime.status_message(load + " BrowserInteration :: " + msg)
+        time.sleep(0.1)
+        if up:
+            count += 1
+            if count == 5:
+                up = False
+        else:
+            count -= 1
+            if count == 0:
+                up = True
+
+
+class OpenBrowserCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        global chrome
+
+        if chrome is not None:
+            status("Shutting down Chrome instance.")
+            chrome.quit()
+            chrome = None
+
+        @async
+        def open_chrome():
+            global chrome
+
+            loading("Opening Chrome new instance.")
+            local_chrome = Chrome()
+            home = setting(self, 'home')
+            loading("Loading %s" % home)
+            local_chrome.get(home)
+            status("Chrome is up and running!")
+            chrome = local_chrome
+
+        open_chrome()
+
+
+class ReloadBrowserCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        global chrome
+
+        if chrome is None:
+            warning("Chrome is not running (or detached).")
+            return
+
+        @async
+        def refresh_browser():
+            loading("Refreshing browser")
+            chrome.refresh()
+
+        refresh_browser()
+
+
+class GoToUrlCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        global chrome
+
+        if chrome is None:
+            warning("Chrome is not running.")
+            return
+
+        @async
+        def onDone(str):
+            status("Opening %s" % str)
+            chrome.get(str)
+            status("Loaded %s" % str)
+
+        self.window.show_input_panel('Enter URL', setting(self, 'home'),
+                                     onDone, None, None)
+
+
+class RunJavascriptInBrowserCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global chrome
+
+        if chrome is None:
+            warning("Chrome is not running.")
+            return
+
+        @async
+        def run_javascript():
+            for region in self.view.sel():
+                if region.empty():
+                    continue
+
+                s = self.view.substr(region)
+                status("Executing `%s`" % s)
+                chrome.execute_script(s)
+
+        run_javascript()
+
+
+select_js = """
+    elements = document.querySelectorAll("%s");
+
+    for (var i=0; i<elements.length; i++) {
+        var el = elements[i];
+        el.setAttribute("data-bi-outline", el.style.outline);
+        el.style.outline = "1px solid red";
+    }
+"""
+
+unselect_js = """
+    elements = document.querySelectorAll("%s");
+
+    for (var i=0; i<elements.length; i++) {
+        var el = elements[i];
+        el.style.outline = el.getAttribute("data-bi-outline");
+    }
+"""
+
+old_selector = None
+
+
+def highlight(selector):
+    global chrome
+    global old_selector
+
+    if chrome is None:
+        warning("Chrome is not running.")
+        return
+
+    if old_selector is not None:
+        chrome.execute_script(unselect_js % old_selector)
+
+    chrome.execute_script(select_js % selector)
+    old_selector = selector
+
+
+class HighlightInBrowserCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        global chrome
+
+        if chrome is None:
+            warning("Chrome is not running.")
+            return
+
+        self.window.show_input_panel('Selector', '',
+                                     None, highlight, None)
