c124f1566298226360afcce3ff3855718c7f43a8
Alejandro Piad <alepiad@gmail.com>
2014-03-22 23:11:35 -0400

    Main menu command now handles nested menus gracefully

--- browser_integration_main_menu.py
+++ browser_integration_main_menu.py
@@ -17,59 +17,79 @@ from .browser_integration_play import *
 
 class BrowserIntegrationMainMenuCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        if not browser.connected():
-            main_menu_commands = [
-                BrowserIntegrationLaunchCommand,
-            ]
-        else:
-            main_menu_commands = [
-                BrowserIntegrationReloadCommand,
-                BrowserIntegrationNavigateCommand,
-                BrowserIntegrationExecuteCommand,
+        main_menu_commands = [
+            BrowserIntegrationLaunchCommand,
+            BrowserIntegrationReloadCommand,
+            BrowserIntegrationNavigateCommand,
+            BrowserIntegrationExecuteCommand,
+            ("View", "View and modify text data of current document", [
                 BrowserIntegrationStylesheetsCommand,
-                BrowserIntegrationSelectCommand,
                 BrowserIntegrationSourceCommand,
-            ]
+            ]),
+            ("Interact", "Interact with the document elements.", [
+                BrowserIntegrationSelectCommand,
+                BrowserIntegrationClickCommand,
+                BrowserIntegrationTypeCommand,
+                BrowserIntegrationClassCommand,
+            ]),
+            ("Macro (experimental)", "Record and replay browser interaction.", [
+                BrowserIntegrationRecordCommand,
+                BrowserIntegrationStopCommand,
+                BrowserIntegrationPlayCommand,
+            ])
+        ]
 
-            if not browser.recording:
-                main_menu_commands.append(BrowserIntegrationRecordCommand)
-            else:
-                main_menu_commands.append(BrowserIntegrationStopCommand)
+        def select_command_func(commands):
+            def select_command(i):
+                if i < 0:
+                    return
 
-            main_menu_commands.append(BrowserIntegrationPlayCommand)
+                command = commands[i]
 
-            if browser.old_selector:
-                main_menu_commands.extend([
-                    BrowserIntegrationClickCommand,
-                    BrowserIntegrationTypeCommand,
-                    BrowserIntegrationClassCommand,
-                ])
+                if isinstance(command, tuple):
+                    name, desc, l = command
 
-        def select_command(i):
-            if i < 0:
-                return
+                    @async
+                    def quick_panel():
+                        sublime.active_window().show_quick_panel([
+                            [command_name(item)] +
+                            command_description(item).split("\n")
+                            for item in l
+                        ], select_command_func(l))
 
-            cls = main_menu_commands[i]
-            cmd = command_str(cls)
+                    quick_panel()
 
-            if issubclass(cls, sublime_plugin.ApplicationCommand):
-                sublime.run_command(cmd)
-            elif issubclass(cls, sublime_plugin.WindowCommand):
-                sublime.active_window().run_command(cmd)
-            elif issubclass(cls, sublime_plugin.TextCommand):
-                sublime.active_window().active_view().run_command(cmd)
+                else:
+                    cmd = command_str(command)
+
+                    if issubclass(command, sublime_plugin.ApplicationCommand):
+                        sublime.run_command(cmd)
+                    elif issubclass(command, sublime_plugin.WindowCommand):
+                        sublime.active_window().run_command(cmd)
+                    elif issubclass(command, sublime_plugin.TextCommand):
+                        sublime.active_window().active_view().run_command(cmd)
+
+            return select_command
 
         def command_str(cls):
             name = cls.__name__
             return "browser_integration_" + name[18:-7].lower()
 
         def command_name(cls):
+            if isinstance(cls, tuple):
+                name, desc, l = cls
+                return name
+
             return cls.plugin_name
 
         def command_description(cls):
+            if isinstance(cls, tuple):
+                name, desc, l = cls
+                return desc
+
             return cls.plugin_description
 
         sublime.active_window().show_quick_panel([
             [command_name(cls)] + command_description(cls).split("\n")
             for cls in main_menu_commands
-        ], select_command)
+        ], select_command_func(main_menu_commands))
