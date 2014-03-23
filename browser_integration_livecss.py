98065ddfdd7b07ca6e26ea9f911ddcd9781e0911
Alejandro Piad <alepiad@gmail.com>
2014-03-22 23:57:22 -0400

    Failsafe on LiveCSS for when browser is not connected

--- browser_integration_livecss.py
+++ browser_integration_livecss.py
@@ -9,14 +9,16 @@ inject_embedded_css_js = """
 
 class BrowserIntegrationLiveCss(sublime_plugin.EventListener):
     def on_modified_async(self, view):
+        if not browser.connected():
+            return
+
         content = view.substr(sublime.Region(0, view.size()))
         name = view.name()
 
         if name and name.startswith('embedded-stylesheet-'):
             idx = name[len('embedded-stylesheet-'):].split('.')[0]
 
-            if browser.connected():
-                code = inject_embedded_css_js % (idx, repr(content))
-                print(code)
-                browser.execute(code)
-                status('Live editing CSS')
+            code = inject_embedded_css_js % (idx, repr(content))
+            print(code)
+            browser.execute(code)
+            status('Live editing CSS')
